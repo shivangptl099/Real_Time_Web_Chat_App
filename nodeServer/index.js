4b1dd4aec811b2b976f1806ebbe9387f2d266f8f
Shivang B. Patel <117822313+shivangptl099@users.noreply.github.com>
2024-04-24 15:58:41 +0530

    Update index.js

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -4,22 +4,30 @@ const server = http.createServer();
 const io = require("socket.io")(server, {});
 const users = {};
 
+function updateUserCount() {
+    io.emit('user-count', Object.keys(users).length);
+}
+
 io.on('connection', socket => {
+
+    updateUserCount();
+
     socket.on('new-user-joined', name => {
         console.log("New user", name);
         users[socket.id] = name;
+        updateUserCount();
         socket.broadcast.emit('user-joined', name);
     });
 
     socket.on('send', message => {
       socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
-        // io.emit('receive', { message: message, name: users[socket.id] });
     });
 
     socket.on('disconnect', () => {
         if (users[socket.id]) {
             socket.broadcast.emit('left', users[socket.id]);
             delete users[socket.id];
+            updateUserCount();
         }
     });
 });
